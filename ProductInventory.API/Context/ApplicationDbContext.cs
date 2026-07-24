7517111e1c61e14c9469147afa86b2bc1682df78
Jolibert-git <rossaespinal01@gmail.com>
2026-07-24 19:22:21 -0400

    Complete project setup

---
+++ ProductInventory.API/Context/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductInventory.API.Entities;
+
+namespace ProductInventory.API.Context
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Product> Products { get; set; } = null!;
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.HasKey(p => p.Id);
+                entity.Property(p => p.Name).IsRequired().HasMaxLength(150);
+                entity.Property(p => p.Description).HasMaxLength(500);
+                entity.Property(p => p.Price).HasColumnType("decimal(18,2)");
+                entity.Property(p => p.Category).IsRequired().HasMaxLength(100);
+            });
+        }
+    }
+}
